f1b71f432d0e29470636447cb3f9108a46c239d9
Tijan Tomislav Radoš <tijan.tomislav@gmail.com>
2026-09-11 00:53:25 +0200

    Changed the opacity on the past dates in the calendar and the training list

--- app/page.js
+++ app/page.js
@@ -131,6 +131,7 @@ export default function Home() {
           else if (isUnavailable) { cellStyle = 'bg-raised text-muted border-raised'; marker = 'x'; }
           else if (isMeasurement) { cellStyle = 'bg-yuzu text-outline border-outline font-bold'; marker = '!'; }
           if (dateStr === today) { cellStyle += ' outline-2 outline-momiji'; }
+          else if (dateStr < today) { cellStyle += ' opacity-60'; }
 
           {/* Cell content */ }
           const content = (
@@ -165,15 +166,22 @@ export default function Home() {
             trainings
               .filter(t => t.date.startsWith(`${year}-${monthStr}`))
               .sort((a, b) => a.date.localeCompare(b.date))
-              .map((training) => (
-                <li key={training.id} className={`training-list spotlight ${training.date === today ? 'outline-2 outline-momiji' : ''}`}>
-                  <Link href={`/trainings/${training.id}`} className="flex items-center gap-5 p-1">
-                    <div className="training-list-date">{training.date}</div>
-                    <div className="training-list-number">{trainingNumber(training)}</div>
-                    <div className="training-list-content">{training.type.toLocaleUpperCase()} at {training.time}</div>
-                  </Link>
-                </li>
-              ))}
+              .map((training) => {
+                {/* Depending on the date's status, we choose a cerain style */ }
+                let cellStyle = "";
+                if (training.date === today) { cellStyle += ' outline-2 outline-momiji'; }
+                else if (training.date < today) { cellStyle += ' opacity-60'; }
+
+                return (
+                  <li key={training.id} className={`training-list spotlight ${cellStyle}`}>
+                    <Link href={`/trainings/${training.id}`} className="flex items-center gap-5 p-1">
+                      <div className="training-list-date">{training.date}</div>
+                      <div className="training-list-number">{trainingNumber(training)}</div>
+                      <div className="training-list-content">{training.type.toLocaleUpperCase()} at {training.time}</div>
+                    </Link>
+                  </li>
+                );
+              })}
         </ul>
       </div>
     </main >
